54297b377d6852d5c7f971d34a1be25eb01c1367
AlanPorter <alankai.porter@gmail.com>
2014-06-05 10:51:28 -0400

    comments

--- src/svugame/ToDo.java
+++ src/svugame/ToDo.java
@@ -13,11 +13,14 @@ package svugame;
 public class ToDo {
     
     //Engine
+        //build battle mode
+            //when choice is made it is passed to the model to see if possible
+            //need battle manager
+        //need model manager for display to comunicate with model
         //figure out tilemap scrolling  -DONE
         //figure out start screen and pause screens - 70%
         //overlays for battle and etc
         //improve our blocking code 95%
-        //build battle mode
         //build transitions, or fix lag in transitions
         //create state for beginning a new games
         //create tilemaps for specific cities
