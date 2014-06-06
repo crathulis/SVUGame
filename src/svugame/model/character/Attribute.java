1104af224a9eb052d097071ec73d3a35a14c3a54
alan.whitehurst <alan.whitehurst@I7-529-AW.svu.local>
2014-06-06 10:00:58 -0400

    Changes made in fleshing out the character and associated model classes

--- src/svugame/model/character/Attribute.java
+++ src/svugame/model/character/Attribute.java
@@ -5,10 +5,46 @@ package svugame.model.character;
  * 100. Attributes can be effected by items, environment, situation, etc.
  * @author Lab Admin
  */
-public class Attribute {
+public class Attribute implements AttributeConstants {
     
-    private String name;
+    private int index;
     private int baseValue;
     private int curValue;
+
+    public Attribute(int attribIndex, int baseValue, int curValue) {
+        this.index = attribIndex;
+        this.baseValue = baseValue;
+        this.curValue = curValue;
+    }
+
+    public String getName() {
+        return ATTRIB_NAMES[index];
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getBaseValue() {
+        return baseValue;
+    }
+
+    public void setBaseValue(int baseValue) {
+        this.baseValue = baseValue;
+    }
+
+    public int getCurValue() {
+        return curValue;
+    }
+
+    public void setCurValue(int curValue) {
+        this.curValue = curValue;
+    }
+    
+    
     
 }
