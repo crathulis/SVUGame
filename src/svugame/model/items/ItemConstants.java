af0393dd8f4245f571017e657c73a43037087fca
alan.whitehurst <alan.whitehurst@I7-529-AW.svu.local>
2014-06-06 10:01:56 -0400

    Changes made in fleshing out the character and associated model classes

---
+++ src/svugame/model/items/ItemConstants.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package svugame.model.items;
+
+/**
+ *
+ * @author alan.whitehurst
+ */
+public interface ItemConstants {
+    
+    public static final int RHAND = 0;
+    public static final int LHAND = 1;
+    public static final int RFINGER = 2;
+    public static final int LFINGER = 3;
+    public static final int HEAD = 4;
+    public static final int NECK = 5;
+    public static final int BODY = 6;
+    public static final int FEET = 7;
+    
+    public static final int NUM_EQUIP = 8;
+}
