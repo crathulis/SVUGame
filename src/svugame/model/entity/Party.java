156089cdb531894de05da5d205c8f9dfba404d5e
Alan <Alan@192.168.1.137>
2014-06-09 22:02:19 -0400

    Added  a class to represent parties.

--- src/svugame/model/entity/Party.java
+++ src/svugame/model/entity/Party.java
@@ -1,19 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package svugame.model.entity;
 
 import java.util.ArrayList;
 
 /**
+ * A party is a group of entities that travel and fight together.
  *
  * @author alan.whitehurst
  */
 public class Party {
-    
+
     private ArrayList<Entity> members;
+
+    /**
+     * Creates an empty party.
+     */
+    public Party() {
+        members = new ArrayList();
+    }
+
+    /**
+     * Add an entity to a party.
+     */
+    public void add(Entity entity) {
+        members.add(entity);
+    }
     
+    /**
+     * Gets the entities that make up a party.
+     * @return an ArrayList of party members.
+     */
+    public ArrayList<Entity> getMembers(){
+        return members;
+    }
+    /**
+     * Get the total health of all party members.
+     *
+     * @return the total health of all party members.
+     */
+    public int getHealth() {
+        int healthTotal = 0;
+        for (Entity entity : members) {
+            healthTotal += entity.getHealth();
+        }
+        return healthTotal;
+    }
+
+    /**
+     * Returns true if the total health of the party
+     * is greater than 0.
+     * @return true if party health > 0; false otherwise.
+     */
+    public boolean isActive() {
+        return getHealth() > 0;
+    }
+
 }
