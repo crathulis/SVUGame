e40c49bd7eb30ed3659ac3f21af761ad92358082
Alan <Alan@192.168.1.137>
2014-06-14 14:49:52 -0400

    Added the concept of actions with success and failure.

--- src/svugame/model/items/Item.java
+++ src/svugame/model/items/Item.java
@@ -6,11 +6,13 @@
 
 package svugame.model.items;
 
+import svugame.model.Thing;
+
 /**
  *
  * @author Lab Admin
  */
-public class Item {
+public class Item extends Thing {
     
     private String name;
     private String description;
@@ -19,5 +21,63 @@ public class Item {
     private int value;
     private int weight;
     private int damage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public void setSlot(int slot) {
+        this.slot = slot;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    
+    
     
 }
