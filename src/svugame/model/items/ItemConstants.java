d69e07e0ff3ef7dd75d309ddfb5eacaa35aec943
Alan <Alan@192.168.1.137>
2014-06-14 07:00:36 -0400

    Added representation for items.

--- src/svugame/model/items/ItemConstants.java
+++ src/svugame/model/items/ItemConstants.java
@@ -12,14 +12,27 @@ package svugame.model.items;
  */
 public interface ItemConstants {
     
-    public static final int RHAND = 0;
-    public static final int LHAND = 1;
-    public static final int RFINGER = 2;
-    public static final int LFINGER = 3;
-    public static final int HEAD = 4;
-    public static final int NECK = 5;
-    public static final int BODY = 6;
-    public static final int FEET = 7;
+    public static final int ITEM_SLOT_RHAND = 0x1;
+    public static final int ITEM_SLOT_LHAND = 0x2;
+    public static final int ITEM_SLOT_RFINGER = 0x4;
+    public static final int ITEM_SLOT_LFINGER = 0x8;
+    public static final int ITEM_SLOT_HEAD = 0x10;
+    public static final int ITEM_SLOT_NECK = 0x20;
+    public static final int ITEM_SLOT_TORSO = 0x40;
+    public static final int ITEM_SLOT_ARMS = 0x80;
+    public static final int ITEM_SLOT_LEGS = 0x100;
+    public static final int ITEM_SLOT_FEET = 0x200;
     
-    public static final int NUM_EQUIP = 8;
+    public static final int NUM_EQUIP = 10;
+    
+    public static final int ITEM_TYPE_WEAPON = 0;
+    public static final int ITEM_TYPE_HELM = 1;
+    public static final int ITEM_TYPE_RING = 2;
+    public static final int ITEM_TYPE_NECKLASS = 3;
+    public static final int ITEM_TYPE_SHIRT = 4;
+    public static final int ITEM_TYPE_BRACER = 5;
+    public static final int ITEM_TYPE_PANTS = 6;
+    public static final int ITEM_TYPE_SHOES = 7;
+    
+    
 }
