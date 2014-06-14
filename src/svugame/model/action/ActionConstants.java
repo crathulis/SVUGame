c1ad3b58c2a021adf77284605a7f890b2b43d393
Alan <Alan@192.168.1.137>
2014-06-14 14:49:12 -0400

    Added the concept of actions with success and failure.

---
+++ src/svugame/model/action/ActionConstants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package svugame.model.action;
+
+/**
+ *
+ * @author Alan
+ */
+public interface ActionConstants {
+    
+    public static final int RESULTS_NONE = 0;
+    public static final int RESULTS_DAMAGE = 1;
+    
+}
