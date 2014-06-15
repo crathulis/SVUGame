a885e4cd4ce4273f29b6b5868f5082e5a4626ede
Alan <Alan@192.168.1.137>
2014-06-14 21:28:01 -0400

    Added the concept of actions with success and failure.

--- src/svugame/model/action/OhThrust.java
+++ src/svugame/model/action/OhThrust.java
@@ -36,7 +36,21 @@ public class OhThrust extends Action implements ItemConstants {
         double agilityChance = actor.getAgility() / 25.0;
         double skillChance = actor.getSkillValue(OH_THRUST);
         int successChance = (int)Math.round(agilityChance + skillChance);
-        return (Dice.roll("1d100")<=successChance);
+        boolean hit = (Dice.roll("1d100")<=successChance);
+        Entity target = (Entity)dobj;
+        boolean dodge = Dice.roll("1d100")<=target.getAgility() - skillChance;
+        if(!hit){
+            System.out.println(actor.getName() + " missed.");
+            return false;
+        } else if(dodge){
+            System.out.println(target.getName() + " dodged.");
+            return false;
+        } else {
+            System.out.println(actor.getName() + " thrusts the " + 
+                    actor.getItemInSlot(ITEM_SLOT_RHAND).getName() +
+                    " into " + target.getName());
+            return true;
+        }
     }
 
     @Override
