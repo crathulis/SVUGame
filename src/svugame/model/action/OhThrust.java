677f722505523b44eb2344a620b9d4a203296bc6
Alan <Alan@192.168.1.137>
2014-06-16 22:00:19 -0400

    Continued to refine the concept of actions with success and failure.

--- src/svugame/model/action/OhThrust.java
+++ src/svugame/model/action/OhThrust.java
@@ -3,10 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package svugame.model.action;
 
-import svugame.model.Dice;
+import svugame.model.Thing;
 import svugame.model.entity.Entity;
 import svugame.model.items.ItemConstants;
 import static svugame.model.skills.SkillConstants.OH_THRUST;
@@ -15,59 +14,36 @@ import static svugame.model.skills.SkillConstants.OH_THRUST;
  *
  * @author Alan
  */
-public class OhThrust extends Action implements ItemConstants {
+public class OhThrust extends MeleeAction implements ItemConstants {
+
+    public OhThrust(Entity actor) {
+        super(actor, OH_THRUST);
+    }
+
+    public OhThrust(Entity actor, Thing dobj) {
+        super(actor, OH_THRUST, dobj);
+    }
+
+    public OhThrust(Entity actor, Thing dobj, Thing iobj) {
+        super(actor, OH_THRUST, dobj, iobj);
+    }
 
     @Override
     public boolean actorCan() {
-        // actor must be holding a one-handed weapon in the right hand
-        if(actor.getItemInSlot(ITEM_SLOT_RHAND).getType()!=ITEM_TYPE_OH_WEAPON){
+        // melee actions require a target (dobj)
+        if (dobj == null) {
+            return false;
+        }
+        // actor must be holding a melee weapon in the right hand
+        if (actor.getItemInSlot(ITEM_SLOT_RHAND).getType() != ITEM_TYPE_OH_WEAPON) {
             return false;
         }
         // dobj must be an entity
-        if(!(dobj instanceof Entity)){
+        if (!(dobj instanceof Entity)) {
             return false;
         }
         // dobj must not be flying or swimming
         return true;
     }
 
-    @Override
-    public boolean success() {
-        double agilityChance = actor.getAgility() / 25.0;
-        double skillChance = actor.getSkillValue(OH_THRUST);
-        int successChance = (int)Math.round(agilityChance + skillChance);
-        boolean hit = (Dice.roll("1d100")<=successChance);
-        Entity target = (Entity)dobj;
-        boolean dodge = Dice.roll("1d100")<=target.getAgility() - skillChance;
-        if(!hit){
-            System.out.println(actor.getName() + " missed.");
-            return false;
-        } else if(dodge){
-            System.out.println(target.getName() + " dodged.");
-            return false;
-        } else {
-            System.out.println(actor.getName() + " thrusts the " + 
-                    actor.getItemInSlot(ITEM_SLOT_RHAND).getName() +
-                    " into " + target.getName());
-            return true;
-        }
-    }
-
-    @Override
-    public int resultType() {
-        return ActionConstants.RESULTS_DAMAGE;
-    }
-
-    @Override
-    public int resultAmount() {
-        double strengthFactor = actor.getStrength() / 25.0;
-        double skillFactor = actor.getSkillValue(OH_THRUST);
-        double weaponDamage = actor.getItemInSlot(ITEM_SLOT_RHAND).getDamage() 
-                * actor.getLevel();
-        return (int)Math.round((strengthFactor + skillFactor) * weaponDamage);
-        
-    }
-    
-    
-    
 }
