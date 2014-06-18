5feae973970f6aefbbfa5317924a95be66b89d66
Lab Admin <Lab Admin@MacLabInst01>
2014-06-18 17:42:17 -0400

    Refining actions and entities.

---
+++ src/svugame/model/skills/SkillFactory.java
@@ -0,0 +1,46 @@
+package svugame.model.skills;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import static svugame.model.skills.SkillConstants.NUM_SKILLS;
+
+/**
+ *
+ * @author Lab Admin
+ */
+class SkillFactory {
+
+    static {
+        final String SKILLLIST_XML = "./data/skillset-jaxb.xml";
+        try {
+            JAXBContext context = JAXBContext.newInstance(SkillList.class);
+            Unmarshaller um = context.createUnmarshaller();
+             skillList = (SkillList) um.unmarshal(new FileReader(SKILLLIST_XML));
+        } catch (JAXBException ex) {
+            Logger.getLogger(SkillFactory.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SkillFactory.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static SkillModel getModel(int skillId) {
+        return skillList.getSkillModelById(skillId);
+    }
+    
+    public static void main(String [] args){
+        for(int i=0;i<NUM_SKILLS;++i){
+            SkillModel sm = SkillFactory.getModel(i);
+            System.out.println(sm);
+        }
+    }
+    
+    private static SkillList skillList;
+
+}
