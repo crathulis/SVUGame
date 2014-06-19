ec74e8a7449dc69fbaa7401d730bcc3222687da2
Lab Admin <Lab Admin@MacLabInst01>
2014-06-19 11:53:02 -0400

    Refining actions and entities.

--- src/svugame/model/action/PlayAction.java
+++ src/svugame/model/action/PlayAction.java
@@ -3,11 +3,12 @@ package svugame.model.action;
 import java.util.ArrayList;
 import svugame.model.Dice;
 import svugame.model.Thing;
+import static svugame.model.action.ActionConstants.RESULTS_NONE;
+import static svugame.model.action.ActionConstants.RESULTS_PLAY;
 import svugame.model.entity.Entity;
 import svugame.model.entity.Party;
 import svugame.model.items.ItemConstants;
 import static svugame.model.skills.SkillConstants.SKILL_PLAY;
-
 
 /**
  *
@@ -26,35 +27,31 @@ public class PlayAction extends Action implements ItemConstants {
     public PlayAction(Entity actor, int skillId, Thing dobj, Thing iobj) {
         super(actor, SKILL_PLAY, dobj, iobj);
     }
-    
+
     @Override
     public boolean isPossible() {
         // actor must be carrying an instrument in one of their hands
-        if(actor.getItemInSlot(ITEM_SLOT_RHAND).getType()!=ITEM_TYPE_INSTRUMENT &&
-                actor.getItemInSlot(ITEM_SLOT_LHAND).getType()!=ITEM_TYPE_INSTRUMENT){
+        if (actor.getItemInSlot(ITEM_SLOT_RHAND).getType() != ITEM_TYPE_INSTRUMENT
+                && actor.getItemInSlot(ITEM_SLOT_LHAND).getType() != ITEM_TYPE_INSTRUMENT) {
             return false;
         }
         // the dobj must be an entity or a party of entities
-        if(!(dobj instanceof Entity) && !(dobj instanceof Party)){
+        if (!(dobj instanceof Entity) && !(dobj instanceof Party)) {
             return false;
         }
         return true;
     }
 
     @Override
-    public boolean isSuccessful() {
+    public ArrayList<Effect> apply() {
+        ArrayList<Effect> results = new ArrayList<>();
         int successChance = actor.getSkillValue(skillId);
-        return (Dice.roll("1d100")<=successChance);
+        if (Dice.roll("1d100") <= successChance) {
+            results.add(new Effect(RESULTS_PLAY, 1, 0));
+        } else {
+            results.add(new Effect(RESULTS_NONE, 0, 0));
+        }
+        return results;
     }
 
-    @Override
-    public ArrayList<Integer> resultType() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ArrayList<Integer> resultAmount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
