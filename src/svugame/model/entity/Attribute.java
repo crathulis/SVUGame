a44ef9267fe4b5b1310ef40f34419b1e8d1d5bfa
Alan <Alan@10.174.57.241>
2014-06-21 19:38:42 -0400

    Continued to refine the concept of actions with success and failure.

--- src/svugame/model/entity/Attribute.java
+++ src/svugame/model/entity/Attribute.java
@@ -1,22 +1,29 @@
 package svugame.model.entity;
 
+import java.util.ArrayList;
+import svugame.model.action.Modifiable;
+import svugame.model.action.Modifier;
+
 /**
  * Attributes describe the capabilities of a player. They range between 1 and
  * 100. Attributes can be effected by items, environment, situation, etc. The
  * base value is the unmodified value of the attribute. The current value allows
  * attributes to be increased or decreased by items, spells, etc. The
  * attributeIndex is defined in AttributeConstants.
+ *
  * @author Lab Admin
  */
-public class Attribute implements AttributeConstants {
-    
+public class Attribute implements AttributeConstants, Modifiable {
+
     private final int index;
     private int baseValue;
     private int curValue;
+    private ArrayList<Modifier> mods;
 
     /**
      * Create a new attribute representing one of the nine attributes for
      * characters.
+     *
      * @param attribIndex the attribute index as defined in AttributeConstants.
      * @param baseValue the baseValue for the attribute.
      * @param curValue the currentValue for the attribute.
@@ -25,10 +32,12 @@ public class Attribute implements AttributeConstants {
         this.index = attribIndex;
         this.baseValue = baseValue;
         this.curValue = curValue;
+        this.mods = new ArrayList<Modifier>();
     }
 
     /**
      * Get the name of the attribute, such as 'strength', or 'wisdom'.
+     *
      * @return the attribute name.
      */
     public String getName() {
@@ -37,6 +46,7 @@ public class Attribute implements AttributeConstants {
 
     /**
      * Get the index for the attribute. This is a value between 0 and 8.
+     *
      * @return the attribute's index.
      */
     public int getIndex() {
@@ -46,6 +56,7 @@ public class Attribute implements AttributeConstants {
     /**
      * Get the base (unmodified) value of the attribute. This does not change
      * once the attribute is created.
+     *
      * @return the attribute's base value.
      */
     public int getBaseValue() {
@@ -53,7 +64,17 @@ public class Attribute implements AttributeConstants {
     }
 
     /**
+     * Sets the base value of the attribute to the specified value.
+     *
+     * @param value the new base value of the attribute.
+     */
+    public void setBaseValue(int value) {
+        baseValue = value;
+    }
+
+    /**
      * Get the current (possibly modified) value of an attribute.
+     *
      * @return the attribute's current value.
      */
     public int getCurValue() {
@@ -62,19 +83,64 @@ public class Attribute implements AttributeConstants {
 
     /**
      * Set the attribute's current value to the new value specified.
+     *
      * @param curValue the new attribute value.
      */
     public void setCurValue(int curValue) {
         this.curValue = curValue;
     }
-    
+
     /**
      * Reset the current value of the attribute to its base value.
      */
     public void resetCurValue() {
         curValue = baseValue;
     }
-    
-    
-    
+
+    @Override
+    public ArrayList<Modifier> getMods() {
+        return mods;
+    }
+
+    @Override
+    public void setMods(ArrayList<Modifier> mods) {
+        this.mods = mods;
+    }
+
+    @Override
+    public void addMods(Modifier mod) {
+        mods.add(mod);
+    }
+
+    @Override
+    public void update() {
+        for (Modifier mod : mods) {
+            if (mod.getDuration() > 0) {
+                mod.setDuration(mod.getDuration() - 1);
+                if (mod.isAdditive()) {
+                    mod.setTotal(mod.getTotal() + mod.getAmount());
+                    curValue += mod.getAmount();
+                }
+            } else if (mod.getDuration() <= 0) {
+                if (mod.isTemporary()) {
+                    curValue -= mod.getTotal();
+                }
+                mods.remove(mod);
+            }
+        }
+
+    }
+
+    @Override
+    public void dispell() {
+        for (Modifier mod : mods) {
+            if (mod.isDispellable()) {
+                if(mod.isTemporary()){
+                    curValue -= mod.getTotal();
+                }
+                mods.remove(mod);
+            }
+        }
+    }
+
 }
