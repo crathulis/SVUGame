9640b7c9ce050707e8afabe3e7ac04750d0e6ec5
Alan <Alan@192.168.1.137>
2014-06-21 06:27:18 -0400

    Continued to refine the concept of actions with success and failure.

--- src/svugame/model/action/Effect.java
+++ src/svugame/model/action/Effect.java
@@ -7,7 +7,11 @@
 package svugame.model.action;
 
 /**
- *
+ * An effect is the result of some action. It may be instantaneous (of duration
+ * 0) or last some number of updates. It may be constant (additive == false) or
+ * accumulate over time (additive == true). If the effect is dispellable, it
+ * can be dispelled through magic.
+ * 
  * @author Alan
  */
 public class Effect {
@@ -15,19 +19,31 @@ public class Effect {
     private int result;
     private int amount;
     private int duration;
+    private boolean additive;
+    private boolean dispellable;
     
     public Effect(int result){
-        this(result, 0, 0);
+        this(result, 0);
     }
 
     public Effect(int result, int amount) {
         this(result, amount, 0);
     }
+    
+    public Effect(int result, int amount, int duration){
+        this(result, amount, duration, false);
+    }
+    
+    public Effect(int result, int amount, int duration, boolean additive){
+        this(result, amount, duration, additive, false);
+    }
 
-    public Effect(int result, int amount, int duration) {
+    public Effect(int result, int amount, int duration, boolean additive, boolean dispellable) {
         this.result = result;
         this.amount = amount;
         this.duration = duration;
+        this.additive = additive;
+        this.dispellable = dispellable;
     }
 
     public int getResult() {
@@ -54,6 +70,25 @@ public class Effect {
         this.duration = duration;
     }
     
+    public boolean isExpired(){
+        return duration<=0;
+    }
+
+    public boolean isAdditive() {
+        return additive;
+    }
+
+    public void setAdditive(boolean additive) {
+        this.additive = additive;
+    }
+
+    public boolean isDispellable() {
+        return dispellable;
+    }
+
+    public void setDispellable(boolean dispellable) {
+        this.dispellable = dispellable;
+    }
     
-    
+       
 }
