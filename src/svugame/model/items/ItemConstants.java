fd0621cac6ef2e0f916a373efb4604f67e5c5a1c
Lab Admin <Lab Admin@MacLabInst01>
2014-06-24 08:59:58 -0400

    Updated Armor constants.

--- src/svugame/model/items/ItemConstants.java
+++ src/svugame/model/items/ItemConstants.java
@@ -39,10 +39,11 @@ public interface ItemConstants {
     public static final int ITEM_TYPE_TH_WEAPON = 11;
     public static final int ITEM_TYPE_RG_WEAPON = 12;
     public static final int ITEM_TYPE_SHIELD = 13;
-    public static final int ITEM_TYPE_ARMOR_NONE = 14;
+    public static final int ITEM_TYPE_ARMOR_CLOTH = 14;
     public static final int ITEM_TYPE_ARMOR_LEATHER = 15;
     public static final int ITEM_TYPE_ARMOR_CHAIN = 16;
-    public static final int ITEM_TYPE_ARMOR_PLATE = 17;
+    public static final int ITEM_TYPE_ARMOR_SCALE = 17;
+    public static final int ITEM_TYPE_ARMOR_PLATE = 18;
     
     
     
