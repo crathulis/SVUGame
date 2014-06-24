7dec6d8eeb2ddcf049bfca78b998acb3e3614f62
Kevin <Kevin@Kevin>
2014-06-24 10:30:27 -0400

    Competed MonsterFactory, MonsterList, and MonsterModel. Moved monster.xml to the data file and deleted the monster.xml which was in the project folder.

---
+++ src/svugame/model/entity/MonsterConstants.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package svugame.model.entity;
+
+/**
+ *
+ * @author Kevin
+ */
+public interface MonsterConstants {
+    
+    public static final int MONSTER_BEAR_BEAR = 0;
+    public static final int MONSTER_BEAR_BROWNBEAR = 1;
+    public static final int MONSTER_BEAR_BLACKBEAR = 2;
+    public static final int MONSTER_WOLF_WOLF = 3;
+    public static final int MONSTER_WOLF_MOUNTAINWOLF = 4;
+    public static final int MONSTER_WOLF_WEREWOLF = 5;
+    public static final int MONSTER_ORC_ORC = 6;
+    public static final int MONSTER_ORC_ORCWARRIOR = 7;
+    public static final int MONSTER_ORC_ORCCHIEFTAN = 8;
+    
+    public static final int NUM_MONSTERS = 9;
+    
+}
