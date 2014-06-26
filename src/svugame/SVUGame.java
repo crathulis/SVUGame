544770e7ad2151656d4e10b12c9c476febbffcf4
alanporter <alanporter@192.168.1.131>
2014-06-25 22:16:54 -0400

    Arena State updated and added back button to character creation

--- src/svugame/SVUGame.java
+++ src/svugame/SVUGame.java
@@ -66,7 +66,7 @@ public class SVUGame extends ClientBase<GameData> {
         addState(new City1());
         addState(new CharCreation(this,States.CharCreation));
         addState(new ArenaState(this,States.ArenaState));
-       enterState(11); 
+       enterState(10); 
     }
     
     
