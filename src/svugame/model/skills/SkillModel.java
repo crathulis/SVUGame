8a49ecc4a6bfa1d41f2bb99bce4432576de5c6e4
Lab Admin <Lab Admin@MacLabInst01>
2014-06-27 08:57:00 -0400

    Added a focus cost for skill on a scale of 1-10.

--- src/svugame/model/skills/SkillModel.java
+++ src/svugame/model/skills/SkillModel.java
@@ -1,35 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package svugame.model.skills;
 
 import javax.xml.bind.annotation.XmlType;
 
 /**
- *
+ * SkillModel describes the basic parameters of skills. Everything the player
+ * attempts to do is modeled by a skillCheck. Skills have a level, and most
+ * skills can be trained. Skills are governed by two attributes, which represent
+ * the base chance of success without training. Levels of training improve the
+ * chance of success beyond the base amount. The exercise of a skill requires
+ * the expenditure of focus.
+ * 
  * @author Lab Admin
  */
 //@XmlRootElement(name = "skill")
-@XmlType(propOrder = {"name", "id", "sdesc", "ldesc", "attrib1", "attrib2"})
+@XmlType(propOrder = {"name", "id", "sdesc", "ldesc", "attrib1", "attrib2", "focus"})
 public class SkillModel implements Comparable {
 
     public SkillModel() {
-
+        this(0);
     }
 
     public SkillModel(int id) {
-        this("", id, "", "", 0, 0);
+        this("", id, "", "", 0, 0, 1);
     }
 
-    public SkillModel(String name, int id, String sdesc, String ldesc, int attrib1, int attrib2) {
+    public SkillModel(String name, int id, String sdesc, String ldesc, int attrib1, int attrib2, int focus) {
         this.name = name;
         this.id = id;
         this.sdesc = sdesc;
         this.ldesc = ldesc;
         this.attrib1 = attrib1;
         this.attrib2 = attrib2;
+        this.focus = focus;
 
     }
 
@@ -81,9 +83,25 @@ public class SkillModel implements Comparable {
         this.attrib2 = attrib2;
     }
 
+    public int getFocus() {
+        return focus;
+    }
+
+    public void setFocus(int focus) {
+        this.focus = focus;
+    }
+    
+    @Override
+    public int compareTo(Object o) {
+        if ((o == null) || (!(o instanceof SkillModel))) {
+            return 1;
+        }
+        return this.id - ((SkillModel) o).getId();
+    }
+
     @Override
     public String toString() {
-        return getName();
+        return String.format("<%s: %d, %d, %d, %d >", name, id, attrib1, attrib2, focus);
     }
 
     private String name;
@@ -92,13 +110,8 @@ public class SkillModel implements Comparable {
     private String ldesc;
     private int attrib1;
     private int attrib2;
+    private int focus;
 
-    @Override
-    public int compareTo(Object o) {
-        if ((o == null) || (!(o instanceof SkillModel))) {
-            return 1;
-        }
-        return this.id - ((SkillModel) o).getId();
-    }
+    
 
 }
