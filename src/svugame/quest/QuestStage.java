cbd5f109aa32fde38d3c7ff0e617a86a5c839589
Student <Student@MacLab07-PC>
2014-06-27 10:10:39 -0400

    Quest Manager fleshed out.

--- src/svugame/quest/QuestStage.java
+++ src/svugame/quest/QuestStage.java
@@ -16,7 +16,11 @@ public class QuestStage {
         return stageNumber;
     }
     
-    QuestStage(int stageNumber, String desc){
+    public QuestStage(int stageNumber, String desc){
+        this(stageNumber, desc, 1);
+    }
+    
+    public QuestStage(int stageNumber, String desc, int counter){
         this.stageNumber = stageNumber;
         this.desc = desc;
     }
@@ -28,14 +32,17 @@ public class QuestStage {
     public int checkAdvancers(int thingID, int actionID){
         for(int n = 0; n < advancers.size(); n++){
             if(advancers.get(n).getThingID() == thingID && 
-                    advancers.get(n).getActionResult() == actionID)
-                return advancers.get(n).getStageToAdvanceTo();
+                    advancers.get(n).getActionResult() == actionID){
+                counter--;
+                if(counter == 0) return advancers.get(n).getStageToAdvanceTo();
+            }
         }
         return -1;
     }
     
     private final int stageNumber;
     private final String desc;
+    private int counter;
     private ArrayList<QuestAdvancer> advancers = new ArrayList<>();
     
 }
